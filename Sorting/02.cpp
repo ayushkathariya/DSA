4852ecb20dd26ff41a546fe7d700cb2923b38d6c
Ayush Kathariya <ayushkathariya108@gmail.com>
2026-03-30 13:28:01 +0545

    feat: update bubble sorting

--- Sorting/02.cpp
+++ Sorting/02.cpp
@@ -7,10 +7,20 @@ int main() {
     vector<int> nums = {13, 46, 24, 9, 52, 20, 9};
 
     for (int i = nums.size() - 1; i >= 1; i--) {
+        bool is_sorted = true;
+
         for (int j = 0; j < i; j++) {
             if (nums[j] > nums[j+1]) {
                 swap(nums[j], nums[j+1]);
+
+                if (is_sorted == true) {
+                    is_sorted = false;
+                }
             }
+        }
+
+        if (is_sorted) {
+            break;
         }
     }
 
