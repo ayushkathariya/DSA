c5444f62253d00f42dfb8acbe1c4c7cd17cb1bb6
Ayush Kathariya <ayushkathariya108@gmail.com>
2026-04-02 08:12:26 +0545

    feat: update recursive bubble sort

--- Sorting/05.cpp
+++ Sorting/05.cpp
@@ -8,10 +8,17 @@ void bubble_sort(vector<int>& arr, int n) {
         return;
     }
 
-    for (int i = 0; i < n; i++) {
+    bool is_sorted = true;
+
+    for (int i = 0; i < n - 1; i++) {
         if (arr[i] > arr[i + 1]) {
             swap(arr[i], arr[i + 1]);
+            is_sorted = false;
         }
+    }
+
+    if (is_sorted) {
+        return;
     }
 
     bubble_sort(arr, n - 1);
@@ -20,7 +27,7 @@ void bubble_sort(vector<int>& arr, int n) {
 int main() {
     vector<int> arr = {3, 2, 8, 5, 1, 4, 23};
 
-    bubble_sort(arr, arr.size() - 1);
+    bubble_sort(arr, arr.size());
 
     for (int it : arr) {
         cout << it << endl;
