24a591060b19139d39c02505bc1151d84250fb10
Ayush Kathariya <ayushkathariya108@gmail.com>
2026-04-02 14:10:36 +0545

    feat: update quick sort

--- Sorting/07.cpp
+++ Sorting/07.cpp
@@ -3,56 +3,53 @@ using namespace std;
 
 /* Quick Sort */
 
-// Function to partition the array
 int partition(vector<int>& arr, int low, int high) {
-    // Choose the last element as pivot
-    int pivot = arr[high];
+    int pivotElem = arr[low];
+    int i = low;
+    int j = high;
 
-    // Initialize i to place smaller elements
-    int i = low - 1;
-
-    // Traverse the array
-    for (int j = low; j < high; j++) {
-        // If element is smaller than or equal to pivot
-        if (arr[j] <= pivot) {
-            // Increment i and swap with j
+    while (i < j) {
+        while (arr[i] <= pivotElem && i <= high - 1) {
             i++;
+        }
+
+        while (arr[j] > pivotElem && j >= low + 1) {
+            j--;
+        }
+
+        if (i < j) {
             swap(arr[i], arr[j]);
         }
     }
 
-    // Place pivot in correct position
-    swap(arr[i + 1], arr[high]);
+    swap(arr[low], arr[j]);
 
-    // Return pivot index
-    return i + 1;
+    return j;
 }
 
 void quickSort(vector<int>& arr, int low, int high) {
-    // Base case: if low is not less than high
     if (low < high) {
-        // Find pivot index after partitioning
+
         int pivotIndex = partition(arr, low, high);
 
-        // Recursively sort elements before pivot
         quickSort(arr, low, pivotIndex - 1);
 
-        // Recursively sort elements after pivot
         quickSort(arr, pivotIndex + 1, high);
     }
+
 }
 
-
 int main() {
-    // Sample array
     vector<int> arr = {10, 7, 8, 9, 1, 5};
+    int low = 0;
+    int high = arr.size() - 1;
 
-    // Apply quicksort
-    quickSort(arr, 0, arr.size() - 1);
+    quickSort(arr, low, high);
 
-    // Print sorted array
-    for (int num : arr)
-        cout << num << endl;
+    // output
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << endl;
+    }
 
     return 0;
 }
