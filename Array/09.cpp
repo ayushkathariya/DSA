7f90fda490dcbb61e9b61ad84604300c8b39696e
Ayush Kathariya <ayushkathariya108@gmail.com>
2026-04-09 07:08:28 +0545

    feat: update find the union of two sorted arrays

--- Array/09.cpp
+++ Array/09.cpp
@@ -4,24 +4,46 @@ using namespace std;
 /* Find the union of two sorted arrays. */
 
 vector<int> find_union(vector<int> arr1, vector<int> arr2) {
-    set<int> st;
+    vector<int> union_arr;
+    int m = arr1.size();
+    int n = arr2.size();
+    int i = 0;
+    int j = 0;
 
-    for (int i = 0; i < arr1.size(); i++) {
-        st.insert(arr1[i]);
+    while (i < m && j < n) {
+        if (arr1[i] <= arr2[j]) {
+            if (union_arr.size() == 0 || union_arr.back() != arr1[i]) {
+                union_arr.push_back(arr1[i]);
+            }
+            i++;
+        } else {
+            if (union_arr.size() == 0 || union_arr.back() != arr2[j]) {
+                    union_arr.push_back(arr2[j]);
+            }
+            j++;
+        }
     }
 
-    for (int i = 0; i < arr2.size(); i++) {
-        st.insert(arr2[i]);
+    while (i < m) {
+        if (union_arr.size() == 0 || union_arr.back() != arr1[i]) {
+            union_arr.push_back(arr1[i]);
+        }
+        i++;
     }
 
-    vector<int> new_arr(st.begin(), st.end());
+    while (j < n) {
+        if (union_arr.size() == 0 || union_arr.back() != arr1[j]) {
+            union_arr.push_back(arr2[j]);
+        }
+        j++;
+    }
 
-    return new_arr;
+    return union_arr;
 }
 
 int main() {
-    vector<int> arr1 = {1, 2, 3, 4, 5};
-    vector<int> arr2 = {2, 3, 4, 4, 5};
+    vector<int> arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector<int> arr2 = {2, 3, 4, 4, 5, 11, 12};
 
     vector<int> new_arr = find_union(arr1, arr2);
 
