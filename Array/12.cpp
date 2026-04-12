38a95df7ffd3762f5d3f54bbb7470d5e3371388a
Ayush Kathariya <ayushkathariya108@gmail.com>
2026-04-12 08:36:07 +0545

    feat: update find the number that appears once, and the other numbers twice

--- Array/12.cpp
+++ Array/12.cpp
@@ -4,27 +4,19 @@ using namespace std;
 /* Find the number that appears once, and the other numbers twice */
 
 int find_num(vector<int> nums) {
-    map<int, int> mp;
+    int xorr = 0;
 
     for (int i = 0; i < nums.size(); i++) {
-        mp[nums[i]]++;
+        xorr = xorr ^ nums[i];
     }
 
-    for (auto it : mp) {
-        if (it.second == 1) {
-            return it.first;
-        }
-    }
-
-    return - 1;
+    return xorr;
 }
 
 int main() {
     vector<int> nums = {1, 2, 3, 2, 1};
 
-    int num = find_num(nums);
-
-    cout << num << endl;
+    cout << find_num(nums) << endl;
 
     return 0;
 }
